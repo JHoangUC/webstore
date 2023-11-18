0a0796204fd4c4be3c805b853664fbd8ef74b5d6
JHoangUC <jhoanguc@gmail.com>
2023-11-18 14:34:50 -0800

    CSS - About Section
    Good checkpoint, About section done
    
    - added tab button css
    - background to text area
    - adjusted borders

--- src/app/components/Homepage.jsx
+++ src/app/components/Homepage.jsx
@@ -7,11 +7,11 @@ const Homepage = () => {
 	return (
 		<section className="flex min-h-screen flex-col bg-[#fafafa]">
 			{/* // Featured products section */}
-			<div className=" text-xl/snug md:text-4xl/snug lg:text-5xl/snug bg-[#bebebe] py-8 flex flex-col items-center justify-center ">
-				<h2 className="font-mont font text-center border-b border-black text-black w-6/12">
+			<div className="  bg-[#bebebe] py-8 flex flex-col items-center justify-center ">
+				<h2 className="text-3xl/snug md:text-4xl/snug lg:text-5xl/snug font-light text-center border-b-2 border-black text-black">
 					Featured Products
 				</h2>
-				<p className="text-center font-mont lg:text-4xl/loose md:text-3xl/loose text-lg/loose text-black">PikaSoja</p>
+				<p className="text-center font-light lg:text-4xl/loose md:text-3xl/loose text-xl/loose text-black">PikaSoja</p>
 				<div className="w-[400px] h-[350px] md:w-[400px] md:h-[500px] lg:w-[500px] lg:h-[600px] relative ">
 				<Image
 					objectFit="contain"
